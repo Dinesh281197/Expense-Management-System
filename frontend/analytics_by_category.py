87623d707b79486764539b341e083d2f0398f91d
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-06-11 22:23:59 +0530

    updated requirements.txt, changed the images, added total expenses in analytics tabs, and modified the chart  x-axis label.

--- frontend/analytics_by_category.py
+++ frontend/analytics_by_category.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import requests
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 
 API_URL =   " http://127.0.0.1:8000"
@@ -28,14 +29,31 @@ def analysis_by_category_tab():
                 "Percentage" : [response_data[category]["percentage"] for category in response_data]
             }
 
+        total_expense = sum([response_data[month]["total_amount"] for month in response_data])
+
         df = pd.DataFrame(data)
+        st.title("Expense breakdown by  Category")
+
+        chart = alt.Chart(df).mark_bar().encode(
+            x=alt.X('Category:N', axis=alt.Axis(labelAngle=0)),  # 0° = straight
+            y='Percentage:Q'
+        )
+
+        st.altair_chart(chart, use_container_width=True)
+
         df_sorted = df.sort_values('Percentage', ascending=False)
         df_sorted['Total(₹)'] = df['Total(₹)'].map('{:.2f}'.format)
         df_sorted['Percentage'] = df['Percentage'].map('{:.0f}'.format).astype(str) + '%'
-        st.title("Expense breakdown by  Category")
-        st.bar_chart(data= df.set_index("Category")['Percentage'])
         df_sorted.set_index('Category', inplace=True)
         st.table(df_sorted)
+
+        start_str = start_date.strftime("%d %b %Y")
+        end_str = end_date.strftime("%d %b %Y")
+
+        if start_date == end_date:
+            st.markdown(f"#### 💰 Total Expense for {start_str} : ₹{total_expense:,.2f}")
+        else:
+            st.markdown(f"#### 💰 Total Expense from {start_str} to {end_str}: ₹{total_expense:,.2f}")
 
 
 
--- frontend/analytics_by_month.py
+++ frontend/analytics_by_month.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 API_URL =   "http://127.0.0.1:8000"
 
@@ -8,7 +9,7 @@ def analysis_by_month_tab():
     years = list(range(2000, 2031))
     selected_year = st.selectbox(label = "Select Year", options=years, index=years.index(2024))
 
-    if selected_year:
+    if selected_year and st.button("Get Analytics", key="get_analytics_button"):
         response = requests.get(f"{API_URL}/analytics/{selected_year}")
 
         if response.status_code == 200:
@@ -17,16 +18,31 @@ def analysis_by_month_tab():
             data = {
                 "Month Number": list(response_data.keys()),
                 "Month": [response_data[month]["month_name"] for month in response_data],
-                "Total": [response_data[month]["total_amount"] for month in response_data],
+                "Total(₹)": [response_data[month]["total_amount"] for month in response_data],
                 "Percentage": [response_data[month]["percentage"] for month in response_data]
             }
 
+            total_expense = sum([response_data[month]["total_amount"] for month in response_data])
+
             df = pd.DataFrame(data)
             st.title("Expense breakdown by Month")
-            st.bar_chart(data=df.set_index("Month")['Percentage'])
+            chart = alt.Chart(df).mark_bar().encode(
+                x=alt.X('Month:N', axis=alt.Axis(labelAngle=0)),  # 0° = straight
+                y='Percentage:Q'
+            )
+
+            st.altair_chart(chart, use_container_width=True)
+
+            df['Total(₹)'] = df['Total(₹)'].map('{:.2f}'.format)
+            df['Percentage'] = df['Percentage'].map('{:.0f}'.format).astype(str) + '%'
+
+
             df.set_index('Month Number',inplace=True)
             df.index.name = None
             st.table(df)
+
+            # Display total
+            st.markdown(f"#### 💰 Total Expense for {selected_year} : ₹{total_expense:,.2f}")
         else:
             st.error("no expenses available for the selected year")
 
